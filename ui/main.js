80f5a8ff43ac3da88f50faeff24d9df6d08d86b9
Roshan kumar <roshankumarroshi@gmail.com>
2017-08-15 01:11:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick = function (){
         }
         
     };
-    request.open('GET','http://roshankumarroshi.imad.hasura-app.io/counter',true);
+    request.open('GET','http://roshankumarroshi@gmail.com.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
