0fa6a6111a3f72e1a76600aa33a51c531867b03a
Roshan kumar <roshankumarroshi@gmail.com>
2017-08-15 17:28:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,36 @@ button.onclick = function (){
     request.send(null);
     
 };
+//submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit-btn');
+submit.onclick=function(){
+    //create a request object
+var request= new XMLHttpRequest();
+//capture the response and store it in a variable
+request.onreadystatechange=function(){
+    
+    if(request.readystate===XMLHttpRequest.DONE){
+        if (request.status===200){
+            
+            //capture a list of names and render it as list
+var names=request.responseText;
+names=JSON.parse(names);
+var list='';
+for(var i=0;i<names.length;i++){
+    list+='<li>'+ names[i]+'</li>';
+}
+var ul =document.getElementById('namelist');
+ul.innerHTML=list;
+
+            
+        }
+    }
+};
+
+request.open('GET','http://roshankumarroshi.imad.hasura-app.io/sunmit-name?name='+name,true);
+request.send(null);
+};
+    
+    
