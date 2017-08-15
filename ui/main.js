4ed6a1b2c73c05973d77f9190071d708dac2e096
Roshan kumar <roshankumarroshi@gmail.com>
2017-08-15 17:56:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,5 @@
 var button= document.getElementById('counter');
-button.onclick = function (){
-    var request= new XMLHttpRequest();
-    request.onreadystatechange= function()
-    {
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-            }
-        }
-        
-    };
-    request.open('GET','http://roshankumarroshi.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
+
 //submit name
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
@@ -29,10 +12,9 @@ request.onreadystatechange=function(){
     
     if(request.readystate===XMLHttpRequest.DONE){
         if (request.status===200){
-            
             //capture a list of names and render it as list
-var names=request.responseText;
-names=JSON.parse(names);
+            var names=request.responseText;
+            names=JASON.parse(names);
 var list='';
 for(var i=0;i<names.length;i++){
     list+='<li>'+ names[i]+'</li>';
